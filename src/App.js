31457cd0694ace5ec884584ad36f79145fd6c205
haluskua <haluskua.one@gmail.com>
2021-09-13 01:03:55 +1200

    added some margin and max-width utilities into my sass module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-
 function App() {
   return (
-    <div className="container bg-error text-white">
-     <h1 className="title-primary">No CRA * REACT</h1>
+    <div className="container  dpy-f fdxn-c bg-error text-white br-sm">
+     <h1 className="fs-lg p-md title-primary">CRA = NO GULP</h1>
+     <p className="p-md mw-lg bg-orange m-2">
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sit perspiciatis quam odio quis voluptatibus qui mollitia ipsa eos minima! Earum, corrupti quibusdam.<br />
+     
+     </p>
+     <a href="https://google.com" className="text-white bg-green mw-sm dpy-f  p-md br-sm text-hover-purple">I am a Button</a>
+     <h2 className="fs-xl p-sm">No CRA * REACT</h2>
     </div>
   );
 }
