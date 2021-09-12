d7eebe268cf204720f39a7fe4d5154ca10bc1a4e
haluskua <haluskua.one@gmail.com>
2021-09-12 19:29:32 +1200

    initial set up with sass file structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="container bg-error text-white">
+     <h1>This is the sass setup before the vendor prefixes and gulp watch </h1>
+     
+    </div>
+  );
+}
+
+export default App;
