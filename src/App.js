f248277db05f884d06d05cf40c83e9f18c4f6126
haluskua <haluskua.one@gmail.com>
2021-09-13 10:34:14 +1200

    added background hover css class

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
      Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel sit perspiciatis quam odio quis voluptatibus qui mollitia ipsa eos minima! Earum, corrupti quibusdam.<br />
      
      </p>
-     <a href="https://google.com" className="btn-one text-white bg-green mw-sm dpy-f fs-xl p-lg br-sm text-hover-purple">I am a Button</a>
+     <a href="https://google.com" className="btn-one text-white bg-green mw-sm dpy-f fs-xl p-lg br-sm text-hover-purple bg-hover-orange">I am a Button</a>
      
      <h2 className="fs-xl p-md">No CRA * REACT</h2>
     </div>
