60d4491a3b720b08785a0523f088253bf72b421b
haluskua <haluskua.one@gmail.com>
2021-09-12 21:46:25 +1200

    testing fontsize lg seems reasonable for all device sizes for h1 tag

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div className="container bg-error text-white">
-     <h1 className="title-primary">With CRA you don't need gulp as a task runner. Most of it is taken care of by Babel </h1>
+     <h1 className="title-primary">No CRA * REACT</h1>
     </div>
   );
 }
